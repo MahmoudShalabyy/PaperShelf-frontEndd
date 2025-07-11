ac411bf0fe77559d764855696ff636b5e24c21ed
MahmoudShalabyy <mahmoudelsayedshalabyy4.com>
2025-07-11 23:42:02 +0200

    add vercel.json and update production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
+// export const environment = {
+//     production: true,
+//     apiBase: 'https://your-production-domain.com/',
+//     apiBaseUrl: 'https://your-production-domain.com/',
+//     apiUrlForImgs: 'https://your-production-domain.com/'
+// };
 export const environment = {
     production: true,
-    apiBase: 'https://your-production-domain.com/',
-    apiBaseUrl: 'https://your-production-domain.com/',
-    apiUrlForImgs: 'https://your-production-domain.com/'
+    apiBase: 'https://papershelf-backend-production.up.railway.app/api/',
+    apiBaseUrl: 'https://papershelf-backend-production.up.railway.app/api/v1/',
+    apiUrlForImgs:'https://papershelf-backend-production.up.railway.app/',
 };
