ce57ea7d6c371525a4b3a50eb1a6d5e036183904
MahmoudShalabyy <mahmoudelsayedshalabyy4.com>
2025-07-12 20:02:38 +0200

    fix: remove extra slash from apiBaseUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,8 +5,8 @@
 //     apiUrlForImgs: 'https://your-production-domain.com/'
 // };
 export const environment = {
-    production: true,
-    apiBase: 'https://papershelf-backend-production.up.railway.app/api/',
-    apiBaseUrl: 'https://papershelf-backend-production.up.railway.app/api/v1/',
-    apiUrlForImgs:'https://papershelf-backend-production.up.railway.app/',
+  production: true,
+  apiBase: 'https://papershelf-backend-production.up.railway.app/api',
+  apiBaseUrl: 'https://papershelf-backend-production.up.railway.app/api/v1',
+  apiUrlForImgs: 'https://papershelf-backend-production.up.railway.app',
 };
