7efb68a693945640b5b597ba045299679aa1bcdc
MahmoudShalabyy <mahmoudelsayedshalabyy4.com>
2025-07-13 03:10:39 +0200

    fix: correct output path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,5 +8,5 @@ export const environment = {
   production: true,
   apiBase: 'https://papershelf-backend-production.up.railway.app/api',
   apiBaseUrl: 'https://papershelf-backend-production.up.railway.app/api/v1',
-  apiUrlForImgs: 'https://papershelf-backend-production.up.railway.app',
+  apiUrlForImgs: 'https://papershelf-backend-production.up.railway.app/',
 };
